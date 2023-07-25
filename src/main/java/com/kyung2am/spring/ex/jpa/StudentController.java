69d78d9466a80fda64d9456b9811fd4601a85447
김경남 <lietty@naver.com>
2023-07-25 21:47:04 +0900

    [김경남] 20230725 예제파일

---
+++ src/main/java/com/kyung2am/spring/ex/jpa/StudentController.java
@@ -0,0 +1,18 @@
+package com.kyung2am.spring.ex.jpa;
+
+import org.springframework.stereotype.Controller;
+
+import com.kyung2am.spring.ex.jpa.domain.Student;
+
+@Controller
+public class StudentController {
+
+
+	public String test() {
+		//builder
+		Student student = 
+		
+		return "/jpa/test";
+	}
+	
+}
---
+++ src/main/java/com/kyung2am/spring/ex/jpa/domain/Student.java
@@ -0,0 +1,19 @@
+package com.kyung2am.spring.ex.jpa.domain;
+
+import java.util.Date;
+
+import lombok.Getter;
+
+@Getter
+public class Student {
+
+	// id, 이름, 전화번호, 이메일, 장래희망
+	private int id;
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String dreamJob;
+	private Date createdAt;
+	private Date updatedAt;
+	
+}
